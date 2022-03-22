516c512df52d5438c01042a75eeb3edac6fe9714
RyndyukDanila <ryndyukdanila@gmail.com>
2022-03-22 14:52:13 +0600

    lab-3 task-2 LR

--- modules/regression_models.py
+++ modules/regression_models.py
@@ -3,8 +3,21 @@ import numpy as np
 
 class LR(object):
 	"""docstring for LR"""
-	def __init__(self, arg):
-		self.arg = arg
+	def __init__(self):
+		self._coef = []
+		self._intercept = 1
 
-	def test(self):
-		return self.arg
+	def _concatenate_for_intercept(self, X):
+		X = np.concatenate((np.ones((X.shape[0], 1)), X), axis=1)
+
+	def fit(self, X, y):
+		self._concatenate_for_intercept(X)
+		self._coef, *_ = np.linalg.lstsq(X, y, rcond=None) # least square method
+		self._intercept = self._coef[0]
+
+	def predict(self, test):
+		predictions = []
+		for row in test:
+			yhat = sum(self._coef * row)
+			predictions.append(yhat)
+		return predictions
